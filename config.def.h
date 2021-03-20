59b8077d17580441123115b5740e82fb4ce761ce
MrMip <Miphamster@protonmail.com>
2021-03-20 00:44:56 -0500

    nord theme

--- config.def.h
+++ config.def.h
@@ -9,9 +9,15 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#ffffff", "#282a36" },
-	[SchemeSel] = { "#ffffff", "#bd93f9" },
+	//Nord 
+	[SchemeNorm] = { "#E5E9F0", "#3B4252" },
+	[SchemeSel] = { "#E5E9F0", "#88C0D0" },
 	[SchemeOut] = { "#000000", "#00ffff" },
+	
+	//Dracula 
+//	[SchemeNorm] = { "#ffffff", "#282a36" },
+//	[SchemeSel] = { "#ffffff", "#bd93f9" },
+//	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
 static unsigned int lines      = 10;
