cb66d665fde7e9293068f70b2ff65c2d13b2384b
arecatsreal <Miphamster@protonmail.com>
2022-11-18 04:25:14 -0600

    stuff

--- config.def.h
+++ config.def.h
@@ -4,20 +4,30 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=15"
+	"VictorMono Nerd Font:size=13"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	//Nord 
-//	[SchemeNorm] = { "#E5E9F0", "#3B4252" },
-//	[SchemeSel] = { "#E5E9F0", "#88C0D0" },
-//	[SchemeOut] = { "#000000", "#00ffff" },
+	// [SchemeNorm] = { "#E5E9F0", "#3B4252" },
+	// [SchemeSel] = { "#E5E9F0", "#8FBCBB" },
+	// [SchemeOut] = { "#000000", "#00ffff" },
+	
+	//Duskfox 
+	[SchemeNorm] = { "#E0dEf4", "#232136" },
+	[SchemeSel] = { "#E0dEf4", "#47407D" },
+	[SchemeOut] = { "#000000", "#00ffff" },
 	
 	//Dracula 
-	[SchemeNorm] = { "#ffffff", "#282a36" },
-	[SchemeSel] = { "#ffffff", "#bd93f9" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	//[SchemeNorm] = { "#ffffff", "#282a36" },
+	//[SchemeSel] = { "#ffffff", "#bd93f9" },
+	//[SchemeOut] = { "#000000", "#00ffff" },
+	
+	//Catppuccin
+	//[SchemeNorm] = { "#D9E0EE", "#1E1E2E" },
+	//[SchemeSel] = { "#1E1E2E", "#C9CBFF" },
+	//[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
 static unsigned int lines      = 10;
